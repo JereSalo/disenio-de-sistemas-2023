58f335ce8e8063a1db979273ec8ca47b7b823271
Salacas <80492835+Salacas@users.noreply.github.com>
2023-09-12 00:37:26 -0300

    entrega 4 terminada

--- src/main/java/persistence/DAO/DaoHibernate.java
+++ src/main/java/persistence/DAO/DaoHibernate.java
@@ -96,7 +96,9 @@ public class DaoHibernate<T> implements Dao<T> {
     }
 
     public void eliminarTodo(){
-        //TODO
+
         instanciarEntityManager();
+
+        this.entityManager.close();
     }
 }
